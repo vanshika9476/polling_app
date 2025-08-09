861ed754ab48dab75130bbb312da68bf272ef7c7
Ravendra Singh <ravendras189@gmail.com>
2025-08-09 19:25:30 +0530

    Contents problem resolved

--- Desktop/Polling-App/backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signIn, signUp } = require("../controllers/userController");
-const router = express.Router();
-
-router.post("/signup", signUp);
-router.post("/signin", signIn);
-
-const userRouter = router;
-module.exports = userRouter;
